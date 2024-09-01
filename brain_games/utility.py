5c8217bf5d5cfeeef553eb07c161fa4e45d0ae25
pro0om <phartsuk@gmail.com>
2024-09-01 13:51:49 +0300

    Update MIN_NUMBER, MAX_NUMBER = start,stop in this function

--- brain_games/utility.py
+++ brain_games/utility.py
@@ -2,5 +2,5 @@ import random
 from brain_games.const import MIN_NUMBER, MAX_NUMBER
 
 
-def get_random_num(MIN_NUMBER,MAX_NUMBER):
-    return random.randint(MIN_NUMBER,MAX_NUMBER)
+def get_random_num(start=MIN_NUMBER,stop=MAX_NUMBER):
+    return random.randint(start,stop)
