759376ea2548675c4b6742d7afb60670371204b6
pro0om <phartsuk@gmail.com>
2024-09-02 18:44:28 +0300

    Add scripts/brain_calc.py

--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,9 +1,10 @@
-# from brain_games.games.calm import run_calm_game
-#
-#
-# def main():
-#     run_even_game()
-#
-#
-# if __name__ == '__main__':
-#     main()
+#!/usr/bin/env python3
+from brain_games.games.calc import run_calc_game
+
+
+def main():
+    run_calc_game()
+
+
+if __name__ == '__main__':
+    main()
