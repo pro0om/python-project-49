15790a211ba3654daa4fc9ceaee9d841b826c2d5
pro0om <phartsuk@gmail.com>
2024-09-03 17:20:33 +0300

    Add MATH_OPERATIONS, CALC_INSTRUCTION

--- brain_games/const.py
+++ brain_games/const.py
@@ -3,6 +3,9 @@ MAX_ROUND = 5
 # Numbers for random
 MIN_NUMBER = 1
 MAX_NUMBER = 10
+# Operations
+MATH_OPERATIONS = ('+', '*', '-')
 # Game instructions
 EVEN_INSTRUCTION = ('If you think that number will be even,'
                     ' write - "yes". If not, write - "no"')
+CALC_INSTRUCTION = 'What is the result of the expression? '
