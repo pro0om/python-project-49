6c7cffecb7259abba4dd9ce21d64613c70b083c3
pro0om <phartsuk@gmail.com>
2024-09-11 19:01:16 +0300

    Add instruction for game progression

--- brain_games/const.py
+++ brain_games/const.py
@@ -10,3 +10,4 @@ EVEN_INSTRUCTION = ('If you think that number will be even,'
                     ' write - "yes". If not, write - "no"')
 CALC_INSTRUCTION = 'What is the result of the expression? '
 GCD_INSTRUCTION = 'Find the greatest common divisor of given numbers'
+PROGRESSION_INSTRUCTION = 'What number is missing in the progression?'
