9388c1327cfebb94c8c446a4b7e1d324d7ff608a
pro0om <phartsuk@gmail.com>
2024-09-11 18:31:34 +0300

    Update text

--- brain_games/const.py
+++ brain_games/const.py
@@ -1,8 +1,10 @@
 # How many rounds
 MAX_ROUND = 5
 # Numbers for random
-MIN_NUMBER = 1
-MAX_NUMBER = 10
+MIN_NUMBER = 10
+MAX_NUMBER = 50
+# Operations
+MATH_OPERATIONS = ('+', '*', '-')
 # Game instructions
 EVEN_INSTRUCTION = ('If you think that number will be even,'
                     ' write - "yes". If not, write - "no"')
