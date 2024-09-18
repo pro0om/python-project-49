8a847c156d72936eb79b822f12981688c47d22ed
pro0om <phartsuk@gmail.com>
2024-09-18 18:01:09 +0300

    Fix

--- brain_games/const.py
+++ brain_games/const.py
@@ -10,8 +10,7 @@ MAX_NUMBER = 10
 # Operations
 MATH_OPERATIONS = ('+', '*', '-')
 # Game instructions
-EVEN_INSTRUCTION = ('If you think that number will be even,'
-                    ' write - "yes". If not, write - "no"')
+EVEN_INSTRUCTION = '/Answer "yes" if the number is even, otherwise answer "no"./'
 CALC_INSTRUCTION = 'What is the result of the expression? '
 GCD_INSTRUCTION = 'Find the greatest common divisor of given numbers'
 PROGRESSION_INSTRUCTION = 'What number is missing in the progression?'
