292792f0531fb8c9e2f2b8452530defd6ad61dc2
pro0om <phartsuk@gmail.com>
2024-09-18 18:43:06 +0300

    Fix

--- brain_games/const.py
+++ brain_games/const.py
@@ -12,6 +12,6 @@ MATH_OPERATIONS = ('+', '*', '-')
 # Game instructions
 EVEN_INSTRUCTION = 'Answer "yes" if the number is even, otherwise answer "no".'
 CALC_INSTRUCTION = 'What is the result of the expression? '
-GCD_INSTRUCTION = 'Find the greatest common divisor of given numbers'
+GCD_INSTRUCTION = 'Find the greatest common divisor of given numbers.'
 PROGRESSION_INSTRUCTION = 'What number is missing in the progression?'
 PRIME_INSTRUCTION = 'Answer "yes" if given number is prime. Others - "no"'
