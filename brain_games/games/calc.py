6ece731bef0d462a2ac7eae935def3e51b7e1d41
pro0om <phartsuk@gmail.com>
2024-09-18 18:32:02 +0300

    fix

--- brain_games/games/calc.py
+++ brain_games/games/calc.py
@@ -16,7 +16,7 @@ def get_result_by_math_symbol(number1, number2, math_symbol):
 def get_math_question_and_answer():
     number1, number2 = get_random_num(), get_random_num()
     math_symbol = random.choice(MATH_OPERATIONS)
-    math_expression = f" {number1} {math_symbol} {number2}"
+    math_expression = f"{number1} {math_symbol} {number2}"
     result = get_result_by_math_symbol(number1, number2, math_symbol)
     return math_expression, str(result)
 
